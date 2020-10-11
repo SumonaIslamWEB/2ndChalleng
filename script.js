94aba7a8e8903734d5a7c96835175d1d84811918
SumonaIslamWEB <71719261+SumonaIslamWEB@users.noreply.github.com>
2020-10-11 22:36:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://media.giphy.com/media/yFQ0ywscgobJK/giphy.gif";
+    div.appendChild(image);
+}
